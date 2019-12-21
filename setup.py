3f27a592dec9a6f12e6938facc4ca59fadd2dc8b
KOCMOHABT1 <57076674+KOCMOHABT1@users.noreply.github.com>
2019-12-21 12:18:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,35 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('README.md') as fh:
+    long_description = fh.read()
 
 setup(
     name='Calman',
-    version='beta 0.9',
-    packages=['my_funct'],
-    url='http://phys.msu.ru',
-    license='SCR',
-    author='Дроздов Алексей',
+    version='0.0.1',
+    author='Drozdov Alexey',
     author_email='drozdofflekha1997@yandex.ru',
-    description='Это программа про фильтр Калмана',
-    install_requires=['matplotlib', 'numpy'],
+    description='Calman filtration',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://phys.msu.ru',
+    license='MIT',
+    packages=find_packages(),
+    # If all your code are in a module, use py_modules instead of packages:
+    # py_modules=['ser'],
     scripts=['bin/serpens'],
     entry_points={
         'console_scripts': ['issnake = ser.snake:main'],
         'gui_scripts': ['plotsnake = ser.snake:plot'],
     },
     test_suite='test',
+    install_requires=['numpy>=1.13', 'matplotlib>=2.0,<3.0'],
     classifiers=[
         'Intended Audience :: Science/Research',
         'Intended Audience :: Education',
         'License :: OSI Approved :: MIT License',
         'Topic :: Education',
         'Programming Language :: Python :: 3',
+        # See full list on https://pypi.org/classifiers/
     ],
     keywords='sample science astrophysics',
 )
